6f242d10541841a00e25f4d3e2045cd50daa01b2
MeinolfE <meinolfe@gmatc.matc.edu>
2025-09-13 19:45:12 -0400

    User regis API endpoint

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db.js';
+import { check, validationResult } from 'express-validator';
 
 //init express app
 const app = express();
@@ -24,10 +25,25 @@ app.get('/',(req, res) =>
  * @route POST api/users
  * @desc Register user
  */
-app.post('/api/users', (req, res) => {
+app.post('/api/users',
     
-    console.log(req.body);
-    res.send(req.body);
+    //Validation requests
+    [
+        check('name', 'Please enter your name').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+    ],
+    (req, res) => {
+        
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        
+        return res.status(400).json({errors: errors.array()});
+    }
+    else {
+        
+        return res.send(req.body);
+    }
 });
 
 //Connection listener
